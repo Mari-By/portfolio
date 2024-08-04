2a58e556e86a9ec6313216ed28bfcfb0825601bb
Mary <mariya.sedna@gmail.com>
2024-08-04 20:19:13 -0300

    aboutme ready

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,11 @@
 const hamburger = document.querySelector('.hamburger'),
     menu = document.querySelector('.menu'),
-    close = document.querySelector('.menu_close');
+    closeElem = document.querySelector('.menu_close');
 
 hamburger.addEventListener('click' , () => {
     menu.classList.add('active');
 });
 
-close.addEventListener('click' , () => {
+closeElem.addEventListener('click' , () => {
     menu.classList.remove('active');
 });
